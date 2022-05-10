0d1c87490609810df29c3659dbb87779ed948343
Megh Gandhi <coolmegh94@gmail.com>
2022-05-10 07:40:49 -0300

    update package.json and build files

--- dist/src/environments/production.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.environment = void 0;
-exports.environment = {
-    production: true,
-    apiUrl: "https://www.omdbapi.com/",
-    apiKey: "dc72febf"
-};
--- dist/src/utils/functions.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getMutFilmPoster = void 0;
-const getMutFilmPoster = (poster) => {
-    return poster !== "N/A" ? poster.replace(/_SX(\d+)/g, "_SX800") : "https://critics.io/img/movies/poster-placeholder.png";
-};
-exports.getMutFilmPoster = getMutFilmPoster;
